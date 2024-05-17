86f37c6c3aee96e2a1266f9e317431762674dd1a
Muhammad Muaz <21100231@lums.edu.pk>
2024-05-17 23:56:47 +0000

    Fixed minor bug

--- tomesv3d_eval.sh
+++ tomesv3d_eval.sh
@@ -4,11 +4,14 @@
 tomesd_token_ratios=(0.1 0.2 0.3 0.4 0.5 0.6 0.75)
 
 for ratio in ${tomesd_token_ratios[@]}; do
+    # Print a dashed line containing the current tomesd token merging ratio
+    echo "--------------------------------------------------"
     echo "Running SV3D-u inference using tomesd token merging ratio: ${ratio}"
+    echo "--------------------------------------------------"
     python scripts/sampling/simple_video_sample.py \
         --input_path ~/sv3d_test \
         --version sv3d_u \
-        --output_path /blob/zyhe/muaz/output/sv3d_u \
+        --output_folder /blob/zyhe/muaz/output/sv3d_u \
         --tomesd_ratio ${ratio} \
         --verbose True
 done
